93d6f0e591706fdb047d9c688c88862df23070d9
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2023-03-02 20:20:00 +0300

    Fragments and bottomNavView

--- app/src/main/java/ru/eco/automan/MainActivity.kt
+++ app/src/main/java/ru/eco/automan/MainActivity.kt
@@ -1,7 +1,10 @@
 package ru.eco.automan
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import ru.eco.automan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navView = binding.bottomNavView
+        navView.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
--- app/src/main/java/ru/eco/automan/Pdd_rules_book.kt
+++ app/src/main/java/ru/eco/automan/Pdd_rules_book.kt
@@ -5,24 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.eco.automan.databinding.FragmentMainPageBinding
+import ru.eco.automan.databinding.FragmentPddRulesBookBinding
 
 
-class MainPage : Fragment() {
-    private var _binding: FragmentMainPageBinding? = null
+class Pdd_rules_book : Fragment() {
+    private var _binding: FragmentPddRulesBookBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMainPageBinding.inflate(inflater, container, false)
+        _binding = FragmentPddRulesBookBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.Hello.text = "Hello from Main Fragment 😊"
+        binding.Hello.text = "Hello from Rules Fragment 😊"
     }
 
     override fun onDestroyView() {
